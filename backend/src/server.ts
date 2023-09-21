b71b007ec75ab017bfdddfd2dfce1a3909c84ab9
Shiran.Ahmed <Shiran.Ahmed@ril.com>
2023-09-21 19:10:27 +0530

    mangoose connectted

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,19 @@
+import "dotenv/config";
+import env from "./util/validateenv";
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello shiran, leraning MERN speedly");
 });
-
-app.listen(port!, () => {
-  console.log("Server running on port: " + port);
-});
+const port = env.PORT;
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("mongoose connected");
+    app.listen(port, () => {
+      console.log("Server running on port: " + port);
+    });
+  })
+  .catch(console.error);
