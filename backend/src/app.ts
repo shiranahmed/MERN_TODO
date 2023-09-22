ce0bfc4caedb6399fdfa67c6658cbc5dbc048c3c
Shiran.Ahmed <Shiran.Ahmed@ril.com>
2023-09-22 19:21:47 +0530

    acees to db done

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import express from "express";
+import noteModel from "./models/note";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello shiran, leraning MERN speedly");
+app.get("/", async (req, res) => {
+  const notes = await noteModel.find().exec();
+  res.status(200).json(notes);
 });
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,13 @@
 import app from "./app";
-import env from "./util/validateenv";
+import env from "./util/validateEnv";
 import mongoose from "mongoose";
 
 const port = env.PORT;
+
 mongoose
   .connect(env.MONGO_CONNECTION_STRING)
   .then(() => {
-    console.log("mongoose connected");
+    console.log("Mongoose connected");
     app.listen(port, () => {
       console.log("Server running on port: " + port);
     });
