f3ceeb04f580e04c9c7be17462f3391e8764b8c3
Shiran.Ahmed <Shiran.Ahmed@ril.com>
2023-09-25 18:52:55 +0530

    handle errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,28 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import noteModel from "./models/note";
+import { Error } from "mongoose";
 const app = express();
 
-app.get("/", async (req, res) => {
-  const notes = await noteModel.find().exec();
-  res.status(200).json(notes);
+app.get("/", async (req, res, next) => {
+  try {
+    const notes = await noteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  next(Error);
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "An unknown error occurred";
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
