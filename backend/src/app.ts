63a53936f663dbcd3dc727355bf39099a6b64f26
Shiran.Ahmed <Shiran.Ahmed@ril.com>
2023-09-26 17:29:05 +0530

    API added for get notes and create notes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,18 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import noteModel from "./models/note";
-import { Error } from "mongoose";
+import notesRoutes from "./routes/notes";
+import morgan from "morgan";
+
 const app = express();
 
-app.get("/", async (req, res, next) => {
-  try {
-    const notes = await noteModel.find().exec();
-    res.status(200).json(notes);
-  } catch (error) {
-    next(error);
-  }
-});
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
-  next(Error);
+  next(Error("End point not found"));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
